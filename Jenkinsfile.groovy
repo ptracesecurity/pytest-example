adf0d609542aff5008037061e63589b13bccc431
mgajewski73 <37601146+mgajewski73@users.noreply.github.com>
2024-01-21 00:44:43 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,22 +27,33 @@ pipeline {
                     def containerId = sh(script: 'docker compose ps -q test-service', returnStdout: true).trim()
                     sh "docker cp ${containerId}:/code/tests/results/. ."
 
-                    junit healthScaleFactor: 5.0, testResults: 'report.xml'
-                    sh 'cp coverage.xml myapp/'
+                    junit healthScaleFactor: 5.0,
+                        testResults: 'report.xml',
+                        keepLongStdio: true,
+                        keepProperties: true,
+                        skipPublishingChecks: true
 
-                    cobertura autoUpdateHealth: false,
-                        autoUpdateStability: false,
-                        coberturaReportFile: 'myapp/coverage.xml',
-                        conditionalCoverageTargets: '70, 0, 0',
-                        enableNewApi: false,
-                        failUnhealthy: false,
-                        failUnstable: false,
-                        lineCoverageTargets: '80, 0, 0',
-                        maxNumberOfBuilds: 0,
-                        methodCoverageTargets: '80, 0, 0',
-                        onlyStable: false,
-                        sourceEncoding: 'ASCII',
-                        zoomCoverageChart: false
+                    recordCoverage skipPublishingChecks: true,
+                        tools: [[
+                            parser: 'COBERTURA',
+                            pattern: 'myapp/coverage.xml'
+                        ]]
+
+//                     sh 'cp coverage.xml myapp/'
+//
+//                     cobertura autoUpdateHealth: false,
+//                         autoUpdateStability: false,
+//                         coberturaReportFile: 'myapp/coverage.xml',
+//                         conditionalCoverageTargets: '70, 0, 0',
+//                         enableNewApi: true,
+//                         failUnhealthy: false,
+//                         failUnstable: false,
+//                         lineCoverageTargets: '80, 0, 0',
+//                         maxNumberOfBuilds: 0,
+//                         methodCoverageTargets: '80, 0, 0',
+//                         onlyStable: false,
+//                         sourceEncoding: 'ASCII',
+//                         zoomCoverageChart: false
                     publishHTML([
                         allowMissing: false,
                         alwaysLinkToLastBuild: true,
